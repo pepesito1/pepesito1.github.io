642b86912fb0884d8325fd0f2e7a62b9a2188e1a
pepesito1 <99051759+pepesito1@users.noreply.github.com>
2022-04-22 13:49:12 -0500

    does the button work?

--- js/darkbutton.js
+++ js/darkbutton.js
@@ -1,11 +1,15 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+			// Wait for document to load
+		document.addEventListener("DOMContentLoaded", function(event) {
+			//store in a variable whatever the data-theme is in the localStorage, if it doesnt exist, then it gets set to "light"
 			var localVariable = localStorage.getItem("data-theme")??'light';
-			document.documentElement.setAttribute("data-theme", localVariable);
-			var themeSwitcher = document.getElementById("theme-switcher");
-				themeSwitcher.onclick = function() {
 					var currentTheme = document.documentElement.getAttribute("data-theme");
+					
+					// Switch between `dark` and `light`
 					var switchToTheme = currentTheme === "dark" ? "light" : "dark";
+					
+					//store in the localStorage whatever the data-theme's value is (the currentTheme)			
 					localStorage.setItem("data-theme", switchToTheme);
+					// Set our current theme to the new one
 					document.documentElement.setAttribute("data-theme", switchToTheme);
-				}
+					}
 		  );
