47707d0e09b4eac19e091ee6a5621e62bf9debd3
pepesito1 <99051759+pepesito1@users.noreply.github.com>
2022-04-22 18:11:00 -0500

    trying to set local variables

--- js/langswitcher-commented.js
+++ js/langswitcher-commented.js
@@ -1,28 +1,31 @@
 var langSwitcher = document.getElementById("languageSwitcher"); //fetch the button, store it on langSwitcher
-		
 		var onLangSwitch = function() {
 			var lang = document.getElementById("lang").value; //get whatever was chosen in the label option select thing
 			
 			var bodyOgandEn; //body to use for original and spy code languages
 			var fontFont; //here we will store the font
 			
-			document.getElementById("mainBodyContent").innerHTML = contentOg; //displays contentOg by default
+			var localLangVariable = localStorage.getItem("contentLang")??'contentOg'; //gets the contentLang from localStorage
+			document.getElementById("mainBodyContent").innerHTML = contentLang; //displays contentOg by default
 			if (lang === "original") {
                 document.getElementById("mainBodyContent").innerHTML = contentOg; //sets the mainBodyContent content t o whatever was in contentOG, same for the rest
-				fontSizey = "100" //fontSizey is in percentage
-				
+				fontSizey = "100" //fontSizey is in percentage	
+				localStorage.setItem("contentLang", lang); //sets the contentLang to the current language
 			} else if (lang === "es") {
 				document.getElementById("mainBodyContent").innerHTML = contentEs;
-				fontSizey= "100"
+				fontSizey= "100";
+				localStorage.setItem("contentLang", lang); //sets the contentLang to the current language
 			} else if (lang === "en") {
 				document.getElementById("mainBodyContent").innerHTML = contentEn;
-				fontSizey = "100"
+				fontSizey = "100";
+				localStorage.setItem("contentLang", lang); //sets the contentLang to the current language
 			} else if (lang ==="secret"){ //sets the fontFont to the secretFont file, sets the fontSizey to 180
-			   document.getElementById("mainBodyContent").innerHTML = contentOg;
+			    document.getElementById("mainBodyContent").innerHTML = contentOg;
 				fontFont = "secretFont";
-				fontSizey = "180"
+				fontSizey = "180";
+				localStorage.setItem("contentLang", lang); //sets the contentLang to the current language
 			}
 			document.getElementById("mainBodyContent").style.fontFamily = fontFont; //sets the font to each respective font
 			document.getElementById("mainBodyContent").style.fontSize = parseInt(fontSizey) + "%"; //here it turns fontSizey to an integer, makes it a percentage
 		  };
-		langSwitcher.addEventListener("click", onLangSwitch);
+langSwitcher.addEventListener("click", onLangSwitch);
