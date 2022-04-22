6489440dfc6a44b103fba692d841aeb3268224e5
pepesito1 <99051759+pepesito1@users.noreply.github.com>
2022-04-22 18:22:29 -0500

    Update langswitcher.js

--- js/langswitcher.js
+++ js/langswitcher.js
@@ -6,24 +6,24 @@ var langSwitcher = document.getElementById("languageSwitcher"); //fetch the butt
 			var fontFont; //here we will store the font
 			
 			var localLangVariable = localStorage.getItem("contentLang")??'contentOg'; //gets the contentLang from localStorage, if it doesnt exist its contentOg, renders the page with that
-			document.getElementById("mainBodyContent").innerHTML = contentLang; //displays contentOg by default
+			document.getElementById("mainBodyContent").innerHTML = localLangVariable; //displays contentOg by default, once a contentLang has been stored it will use that
 			if (lang === "original") {
                 document.getElementById("mainBodyContent").innerHTML = contentOg; //sets the mainBodyContent content t o whatever was in contentOG, same for the rest
 				fontSizey = "100" //fontSizey is in percentage	
-				localStorage.setItem("contentLang", lang); //sets the contentLang to the current language
+				localStorage.setItem("contentLang", "contentOg"); //sets the contentLang to the current language
 			} else if (lang === "es") {
 				document.getElementById("mainBodyContent").innerHTML = contentEs;
 				fontSizey= "100";
-				localStorage.setItem("contentLang", lang); //sets the contentLang to the current language
+				localStorage.setItem("contentLang", "contentEs"); //sets the contentLang to the current language
 			} else if (lang === "en") {
 				document.getElementById("mainBodyContent").innerHTML = contentEn;
 				fontSizey = "100";
-				localStorage.setItem("contentLang", lang); //sets the contentLang to the current language
+				localStorage.setItem("contentLang", "contentEn"; //sets the contentLang to the current language
 			} else if (lang ==="secret"){ //sets the fontFont to the secretFont file, sets the fontSizey to 180
 			    document.getElementById("mainBodyContent").innerHTML = contentOg;
 				fontFont = "secretFont";
 				fontSizey = "180";
-				localStorage.setItem("contentLang", lang); //sets the contentLang to the current language
+				localStorage.setItem("contentLang", "contentOg"); //sets the contentLang to the current language
 			}
 			document.getElementById("mainBodyContent").style.fontFamily = fontFont; //sets the font to each respective font
 			document.getElementById("mainBodyContent").style.fontSize = parseInt(fontSizey) + "%"; //here it turns fontSizey to an integer, makes it a percentage
