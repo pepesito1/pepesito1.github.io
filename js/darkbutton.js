3dee8847b4b0e8d24fc0aefa3a989a176f56d68f
pepesito1 <99051759+pepesito1@users.noreply.github.com>
2022-04-22 13:21:00 -0500

    does this work?

--- js/darkbutton.js
+++ js/darkbutton.js
@@ -1,4 +1,4 @@
-		// Wait for document to load
+			// Wait for document to load
 		document.addEventListener("DOMContentLoaded", function(event) {
 			//store in a variable whatever the data-theme is in the localStorage, if it doesnt exist, then it gets set to "light"
 			var localVariable = localStorage.getItem("data-theme")??'light';
@@ -15,7 +15,6 @@
 					var currentTheme = document.documentElement.getAttribute("data-theme");
 					
 					// Switch between `dark` and `light`
-					
 					var switchToTheme = currentTheme === "dark" ? "light" : "dark";
 					
 					//store in the localStorage whatever the data-theme's value is (the currentTheme)			
