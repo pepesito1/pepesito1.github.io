317fe1a8493cd24c92f512383f1e6f19fe5ce21e
pepesito1 <99051759+pepesito1@users.noreply.github.com>
2022-04-22 19:47:57 -0500

    added localvariables, again

--- js/langswitcher.js
+++ js/langswitcher.js
@@ -1,6 +1,7 @@
 	//esto funciona asi
 	
 	var langSwitcher = document.getElementById("languageSwitcher"); //fetches the button, stores it on langSwitcher
+	var localVariable = localStorage.getItem("localContentLang")??'contentOg'; //grabs localContentLang, if it doesnt exist its set to contentOg
 	document.getElementById("mainBodyContent").innerHTML = contentOg; //displays contentOg by default
 	var onLangSwitch = function() {
 		var lang = document.getElementById("lang").value; //lang is equal to the label button
@@ -8,16 +9,20 @@
 			if (lang === "original") {
 				document.getElementById("mainBodyContent").innerHTML = contentOg; //contentOg is defined in the html
 				fontSizey = "100";
+				localStorage.setItem("localContentLang", "contentOg"); //sets to localContentLang the contentOg
 			} else if (lang === "es") {
 				document.getElementById("mainBodyContent").innerHTML = contentEs; //contentEs is defined on the html
 				fontSizey= "100";
+				localStorage.setItem("localContentLang", "contentEs"); //sets to localContentLang the contentEs
 			} else if (lang === "en") {
 				document.getElementById("mainBodyContent").innerHTML = contentEn; //content En is defined on the html
 				fontSizey = "100";
+				localStorage.setItem("localContentLang", "contentEn"); //sets to localContentLang the contentEn
 			} else if (lang ==="secret"){
 				document.getElementById("mainBodyContent").innerHTML = contentOg; //contentOg is defined in the html
 				fontFont = "secretFont";
 				fontSizey = "180";
+				localStorage.setItem("localContentLang", "contentOg"); //sets to localContentLang the contentOg
 			}
 			document.getElementById("mainBodyContent").style.fontFamily = fontFont; //sets font
 			document.getElementById("mainBodyContent").style.fontSize = parseInt(fontSizey) + "%"; //makes the fontSizey an integer, adds a %
