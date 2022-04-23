b9cd21f09f7eca1bc9d0b144d2e5cb186cc8a509
pepesito1 <99051759+pepesito1@users.noreply.github.com>
2022-04-23 15:20:35 -0500

    changingHeader

--- js/langswitcher.js
+++ js/langswitcher.js
@@ -20,18 +20,22 @@
 		var fontFont;
 			if (lang === "original") {
 				document.getElementById("mainBodyContent").innerHTML = contentOg; //contentOg is defined in the html
+				document.getElementById("header").innerHTML = headerContentOg; //grabs the header, headerContent is defined in the html
 				fontSizey = "100";
 				localStorage.setItem("localContentLang", "contentOg"); //sets to localContentLang the contentOg
 			} else if (lang === "es") {
 				document.getElementById("mainBodyContent").innerHTML = contentEs; //contentEs is defined on the html
+				document.getElementById("header").innerHTML = headerContentEs; //grabs the header, headerContent is defined in the html
 				fontSizey= "100";
 				localStorage.setItem("localContentLang", "contentEs"); //sets to localContentLang the contentEs
 			} else if (lang === "en") {
 				document.getElementById("mainBodyContent").innerHTML = contentEn; //content En is defined on the html
+				document.getElementById("header").innerHTML = headerContentEn; //grabs the header, headerContent is defined in the html
 				fontSizey = "100";
 				localStorage.setItem("localContentLang", "contentEn"); //sets to localContentLang the contentEn
 			} else if (lang ==="secret"){
 				document.getElementById("mainBodyContent").innerHTML = contentOg; //contentOg is defined in the html
+				document.getElementById("header").innerHTML = headerContentOg; //grabs the header, headerContent is defined in the html
 				fontFont = "secretFont";
 				fontSizey = "180";
 				localStorage.setItem("localContentLang", "contentSpy"); //sets to localContentLang the contentSpy
