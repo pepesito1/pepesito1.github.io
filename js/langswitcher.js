b8f551926c98977f083c00e735de89eeab72251f
pepesito1 <99051759+pepesito1@users.noreply.github.com>
2022-04-22 19:38:00 -0500

    reverting languageswitcher

--- js/langswitcher.js
+++ js/langswitcher.js
@@ -1,32 +1,23 @@
-var langSwitcher = document.getElementById("languageSwitcher"); //fetch the button, store it on langSwitcher
-var localLang = localStorage.getItem("localLang")??'contentOg'; //gets the localLang from localStorage, if it doesnt exist its contentOg, renders the page with that
-document.getElementById("mainBodyContent").innerHTML = localLang; //displays contentOg by default, once a localLang has been stored it will use that
-
+	var langSwitcher = document.getElementById("languageSwitcher");
 	var onLangSwitch = function() {
-		var lang = document.getElementById("lang").value; //get whatever was chosen in the label option select thing
-			
-		var bodyOgandEn; //body to use for original and spy code languages
-		var fontFont; //here we will store the font
-			
-		if (lang === "contentOg") {
-            document.getElementById("mainBodyContent").innerHTML = contentOg; //sets the mainBodyContent content to whatever was in contentOg, same for the rest
-			fontSizey = "100" //fontSizey is in percentage	
-			localStorage.setItem("localLang", "contentOg"); //sets the localLang to the current language
-		} else if (lang === "contentEs") {
+	var lang = document.getElementById("lang").value;
+	var bodyOgandEn;
+	var fontFont;
+		if (lang === "original") {
+            document.getElementById("mainBodyContent").innerHTML = contentOg;
+			fontSizey = "100";
+		} else if (lang === "es") {
 			document.getElementById("mainBodyContent").innerHTML = contentEs;
 			fontSizey= "100";
-			localStorage.setItem("localLang", "contentEs"); //sets the localLang to the current language
-		} else if (lang === "contentEn") {
+		} else if (lang === "en") {
 			document.getElementById("mainBodyContent").innerHTML = contentEn;
 			fontSizey = "100";
-			localStorage.setItem("localLang", "contentEn"); //sets the localLang to the current language
-		} else if (lang ==="contentOg"){ //sets the fontFont to the secretFont file, sets the fontSizey to 180
-		    document.getElementById("mainBodyContent").innerHTML = contentOg;
+		} else if (lang ==="secret"){
+			document.getElementById("mainBodyContent").innerHTML = contentOg;
 			fontFont = "secretFont";
 			fontSizey = "180";
-			localStorage.setItem("localLang", "contentOg"); //sets the localLang to the current language
 		}
-		document.getElementById("mainBodyContent").style.fontFamily = fontFont; //sets the font to each respective font
-		document.getElementById("mainBodyContent").style.fontSize = parseInt(fontSizey) + "%"; //here it turns fontSizey to an integer, makes it a percentage
+		document.getElementById("mainBodyContent").style.fontFamily = fontFont;
+		document.getElementById("mainBodyContent").style.fontSize = parseInt(fontSizey) + "%";
+	};
 langSwitcher.addEventListener("click", onLangSwitch);
-	}
