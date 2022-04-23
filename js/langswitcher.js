ca59c1563b156922f38b815cbc6aef4410b147b4
pepesito1 <99051759+pepesito1@users.noreply.github.com>
2022-04-22 19:49:33 -0500

    fixed, it should work now

--- js/langswitcher.js
+++ js/langswitcher.js
@@ -2,7 +2,7 @@
 	
 	var langSwitcher = document.getElementById("languageSwitcher"); //fetches the button, stores it on langSwitcher
 	var localVariable = localStorage.getItem("localContentLang")??'contentOg'; //grabs localContentLang, if it doesnt exist its set to contentOg
-	document.getElementById("mainBodyContent").innerHTML = contentOg; //displays contentOg by default
+	document.getElementById("mainBodyContent").innerHTML = localContentLang; //displays localContentLang
 	var onLangSwitch = function() {
 		var lang = document.getElementById("lang").value; //lang is equal to the label button
 		var fontFont;
