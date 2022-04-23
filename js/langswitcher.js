cd4d75c03a6653550bef1bea590a9b8bd3750c9b
pepesito1 <99051759+pepesito1@users.noreply.github.com>
2022-04-22 19:43:36 -0500

    IT WORKS UNTIL HERE, REEVERT TO HERE IF IT BREAKS

--- js/langswitcher.js
+++ js/langswitcher.js
@@ -1,23 +1,24 @@
-	var langSwitcher = document.getElementById("languageSwitcher");
+	//esto funciona asi
+	
+	var langSwitcher = document.getElementById("languageSwitcher"); //fetches the button, stores it on langSwitcher
 	var onLangSwitch = function() {
-	var lang = document.getElementById("lang").value;
-	var bodyOgandEn;
-	var fontFont;
-		if (lang === "original") {
-            document.getElementById("mainBodyContent").innerHTML = contentOg;
-			fontSizey = "100";
-		} else if (lang === "es") {
-			document.getElementById("mainBodyContent").innerHTML = contentEs;
-			fontSizey= "100";
-		} else if (lang === "en") {
-			document.getElementById("mainBodyContent").innerHTML = contentEn;
-			fontSizey = "100";
-		} else if (lang ==="secret"){
-			document.getElementById("mainBodyContent").innerHTML = contentOg;
-			fontFont = "secretFont";
-			fontSizey = "180";
-		}
-		document.getElementById("mainBodyContent").style.fontFamily = fontFont;
-		document.getElementById("mainBodyContent").style.fontSize = parseInt(fontSizey) + "%";
+		var lang = document.getElementById("lang").value; //lang is equal to the label button
+		var fontFont;
+			if (lang === "original") {
+				document.getElementById("mainBodyContent").innerHTML = contentOg; //contentOg is defined in the html
+				fontSizey = "100";
+			} else if (lang === "es") {
+				document.getElementById("mainBodyContent").innerHTML = contentEs; //contentEs is defined on the html
+				fontSizey= "100";
+			} else if (lang === "en") {
+				document.getElementById("mainBodyContent").innerHTML = contentEn; //content En is defined on the html
+				fontSizey = "100";
+			} else if (lang ==="secret"){
+				document.getElementById("mainBodyContent").innerHTML = contentOg; //contentOg is defined in the html
+				fontFont = "secretFont";
+				fontSizey = "180";
+			}
+			document.getElementById("mainBodyContent").style.fontFamily = fontFont; //sets font
+			document.getElementById("mainBodyContent").style.fontSize = parseInt(fontSizey) + "%"; //makes the fontSizey an integer, adds a %
 	};
-langSwitcher.addEventListener("click", onLangSwitch);
+langSwitcher.addEventListener("click", onLangSwitch); //runs function when button is clicked
