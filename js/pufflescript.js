1f3b8b42f99bda4157aa2e87ebfff784a046a0da
pepesito1 <99051759+pepesito1@users.noreply.github.com>
2022-04-26 13:46:59 -0500

    adding music thing

--- js/pufflescript.js
+++ js/pufflescript.js
@@ -1,17 +1,17 @@
  //puffle keeper script
 	var storedGif = document.getElementById("boilerKeeper"); //we grab the gif and store it in storedboilerKeeper
 	var onGifClick = function() {
-	  if (document.getElementById("boilerKeeper").src.includes("/images/greenKeeper.gif")) {
-		document.getElementById("boilerKeeper").src = "/images/blueKeeper.gif";
+	  if (document.getElementById("boilerKeeper").src.includes("/media/greenKeeper.gif")) {
+		document.getElementById("boilerKeeper").src = "/media/blueKeeper.gif";
 
-	  } else if (document.getElementById("boilerKeeper").src.includes("/images/blueKeeper.gif")) {
-		document.getElementById("boilerKeeper").src = "/images/yellowKeeper.gif";
+	  } else if (document.getElementById("boilerKeeper").src.includes("/media/blueKeeper.gif")) {
+		document.getElementById("boilerKeeper").src = "/media/yellowKeeper.gif";
 
-	  } else if (document.getElementById("boilerKeeper").src.includes("/images/yellowKeeper.gif")) {
-		document.getElementById("boilerKeeper").src = "/images/redKeeper.gif";
+	  } else if (document.getElementById("boilerKeeper").src.includes("/media/yellowKeeper.gif")) {
+		document.getElementById("boilerKeeper").src = "/media/redKeeper.gif";
 
-	  } else if (document.getElementById("boilerKeeper").src.includes("/images/redKeeper.gif")) {
-		document.getElementById("boilerKeeper").src = "/images/greenKeeper.gif";
+	  } else if (document.getElementById("boilerKeeper").src.includes("/media/redKeeper.gif")) {
+		document.getElementById("boilerKeeper").src = "/media/greenKeeper.gif";
 	  }
 	}
 	storedGif.addEventListener("click", onGifClick);
