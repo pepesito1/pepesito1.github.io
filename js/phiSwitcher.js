c05fcb1f8e0948c0e433a3f91f23e1402e60e785
pepesito1 <99051759+pepesito1@users.noreply.github.com>
2022-04-26 13:21:16 -0500

    Update phiSwitcher.js

--- js/phiSwitcher.js
+++ js/phiSwitcher.js
@@ -1,34 +1,31 @@
 	 // modeVariable is either light or dark for the theme, and the storedLocalContent is either contentEn,Es,Og or contentSpyZ
-	phiHrefSwitch = function() {
-	var modeVariable = localStorage.getItem("data-theme")??'light'; //we fetch it again, for some reason. i should use let and the other thingy instead of var
-	var storedLocalContent = localStorage.getItem("localContentLang")??'contentOg';
-		if (modeVariable == "light" &&  storedLocalContent == "contentEn"){
-			document.getElementById("phiLink").href = "/main/phi/enLight/phi.html";
-			
-		} else if (modeVariable == "light" &&  storedLocalContent == "contentEs"){
-			document.getElementById("phiLink").href = "/main/phi/esLight/phi.html";
-			
-		} else if (modeVariable == "light" &&  storedLocalContent == "contentOg"){
-			document.getElementById("phiLink").href = "/main/phi/enLight/phi.html";
-			
-		} else if(modeVariable == "light" &&  storedLocalContent == "contentSpy"){
-			document.getElementById("phiLink").href = "/main/phi/enLight/phi.html";
-			
-		} else if (modeVariable == "light" &&  storedLocalContent == "contentEn"){
-			document.getElementById("phiLink").href = "/main/phi/enLight/phi.html";
-			
-		} else if (modeVariable == "dark" &&  storedLocalContent == "contentEs"){
-			document.getElementById("phiLink").href = "/main/phi/esDark/phi.html";
-			
-		} else if(modeVariable == "dark" &&  storedLocalContent == "contentOg"){
-			document.getElementById("phiLink").href = "/main/phi/enDark/phi.html";
-			
-		} else if(modeVariable == "dark" &&  storedLocalContent == "contentSpy"){
-			document.getElementById("phiLink").href = "/main/phi/enDark/phi.html";
-			
-		} else if(modeVariable == "dark" &&  storedLocalContent == "contentEn"){
+phiHrefSwitch = function() {
+var modeVariable = localStorage.getItem("data-theme")??'light'; //we fetch it again, for some reason. i should use let and the other thingy instead of var
+var storedLocalContent = localStorage.getItem("localContentLang")??'contentOg';
+	if (modeVariable == "light" &&  storedLocalContent == "contentEn"){
+		document.getElementById("phiLink").href = "/main/phi/enLight/phi.html";
+
+	} else if (modeVariable == "light" &&  storedLocalContent == "contentEs"){
+		document.getElementById("phiLink").href = "/main/phi/esLight/phi.html";
+
+	} else if (modeVariable == "light" &&  storedLocalContent == "contentOg"){
+		document.getElementById("phiLink").href = "/main/phi/enLight/phi.html";
+
+	} else if(modeVariable == "light" &&  storedLocalContent == "contentSpy"){
+		document.getElementById("phiLink").href = "/main/phi/enLight/phi.html";
+
+	} else if (modeVariable == "dark" &&  storedLocalContent == "contentEs"){
+		document.getElementById("phiLink").href = "/main/phi/esDark/phi.html";
+
+	} else if(modeVariable == "dark" &&  storedLocalContent == "contentOg"){
 		document.getElementById("phiLink").href = "/main/phi/enDark/phi.html";
-		}};
-	langSwitcher.addEventListener("click", phiHrefSwitch);
-	themeSwitcher.addEventListener("click", phiHrefSwitch);
-	phiHrefSwitch();
+
+	} else if(modeVariable == "dark" &&  storedLocalContent == "contentSpy"){
+		document.getElementById("phiLink").href = "/main/phi/enDark/phi.html";
+
+	} else if(modeVariable == "dark" &&  storedLocalContent == "contentEn"){
+	document.getElementById("phiLink").href = "/main/phi/enDark/phi.html";
+	}};
+langSwitcher.addEventListener("click", phiHrefSwitch);
+themeSwitcher.addEventListener("click", phiHrefSwitch);
+phiHrefSwitch();
