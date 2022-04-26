9c91f17ee054264ecee639ebfbd91bab883609ab
pepesito1 <99051759+pepesito1@users.noreply.github.com>
2022-04-26 13:28:02 -0500

    inverted function lmfao

--- js/phiSwitcher.js
+++ js/phiSwitcher.js
@@ -26,6 +26,38 @@ var storedLocalContent = localStorage.getItem("localContentLang")??'contentOg';
 	} else if(modeVariable == "dark" &&  storedLocalContent == "contentEn"){
 	document.getElementById("phiLink").href = "/main/phi/enDark/phi.html";
 	}};
+
+	//theme switcher doesnt work properly. if the theme is light then it changes the theme to dark due to the way
+	//the functions are ran, this is my fix:
+phiInvertedHrefSwitch = function() {
+var modeVariable = localStorage.getItem("data-theme")??'light'; //we fetch it again, for some reason. i should use let and the other thingy instead of var
+var storedLocalContent = localStorage.getItem("localContentLang")??'contentOg';
+	if (modeVariable == "dark" &&  storedLocalContent == "contentEn"){
+		document.getElementById("phiLink").href = "/main/phi/enLight/phi.html";
+
+	} else if (modeVariable == "dark" &&  storedLocalContent == "contentEs"){
+		document.getElementById("phiLink").href = "/main/phi/esLight/phi.html";
+
+	} else if (modeVariable == "dark" &&  storedLocalContent == "contentOg"){
+		document.getElementById("phiLink").href = "/main/phi/enLight/phi.html";
+
+	} else if(modeVariable == "dark" &&  storedLocalContent == "contentSpy"){
+		document.getElementById("phiLink").href = "/main/phi/enLight/phi.html";
+
+	} else if (modeVariable == "light" &&  storedLocalContent == "contentEs"){
+		document.getElementById("phiLink").href = "/main/phi/esDark/phi.html";
+
+	} else if(modeVariable == "light" &&  storedLocalContent == "contentOg"){
+		document.getElementById("phiLink").href = "/main/phi/enDark/phi.html";
+
+	} else if(modeVariable == "light" &&  storedLocalContent == "contentSpy"){
+		document.getElementById("phiLink").href = "/main/phi/enDark/phi.html";
+
+	} else if(modeVariable == "light" &&  storedLocalContent == "contentEn"){
+	document.getElementById("phiLink").href = "/main/phi/enDark/phi.html";
+	}};
+	//holy shit this code is bad
+	
 langSwitcher.addEventListener("click", phiHrefSwitch);
-themeSwitcher.addEventListener("click", phiHrefSwitch);
+themeSwitcher.addEventListener("click", phiInvertedHrefSwitch);
 phiHrefSwitch();
