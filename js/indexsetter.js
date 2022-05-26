90f383f3c71aeefae58cda96a09c88abfd914eb9
pepesito1 <pepesito1@users.noreply.github.com>
2022-05-25 20:38:36 -0500

    fixing html

--- js/indexsetter.js
+++ js/indexsetter.js
@@ -6,7 +6,7 @@ var indexContent = `
 		<li>blogposts?</li>
 			<ol>
 				<li><a href="/main/blogposts/life29time04spot2022">spot of a lifetime</a></li>
-				<li><a href="/main/blogposts/happ25iest05girl2022">spot of a lifetime</a></li>
+				<li><a href="/main/blogposts/happ25iest05girl2022">pepesito for inkie</a></li>
 			</ol>
 		<li><a href="/main/music">music</a><br></li>
 		<li><a href="/main/images">images</a><br></li>
