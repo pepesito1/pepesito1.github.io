4cec422df3a8c2be4c6df4889b10da9a76f18811
pepesito1 <pepesito1@users.noreply.github.com>
2022-06-16 15:03:16 -0500

    fixing new

--- js/indexsetter.js
+++ js/indexsetter.js
@@ -7,6 +7,7 @@ var indexContent = `
 			<ol>
 				<li><a href="/main/blogposts/life29time04spot2022">spot of a lifetime</a></li>
 				<li><a href="/main/blogposts/happ25iest05girl2022">pepesito for inkie</a></li>
+				<li><a href="/main/blogposts/per16fect06ruiz2022">cloudy day sunny day</a></li>
 			</ol>
 		<li><a href="/main/music">music</a><br></li>
 		<li><a href="/main/images">images</a><br></li>
