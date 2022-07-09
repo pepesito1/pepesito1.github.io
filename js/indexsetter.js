dcd35a951716954d74f80b5191a11c487cc1ef03
pepesito1 <pepesito1@users.noreply.github.com>
2022-07-09 13:28:15 -0500

    new post

--- js/indexsetter.js
+++ js/indexsetter.js
@@ -8,6 +8,7 @@ var indexContent = `
 				<li><a href="/main/blogposts/life29time04spot2022">spot of a lifetime</a></li>
 				<li><a href="/main/blogposts/happ25iest05girl2022">pepesito for inkie</a></li>
 				<li><a href="/main/blogposts/per16fect06ruiz2022">cloudy day sunny day</a></li>
+				<li><a href="/main/blogposts/prelude04tothe07truth2022">white fatalis mask</a></li>
 			</ol>
 		<li><a href="/main/music">music</a><br></li>
 		<li><a href="/main/images">images</a><br></li>
