2a5dbf568b320ac6b06e5c053a3107c6cc2a2136
pepesito1 <pepesito1@users.noreply.github.com>
2023-02-02 16:04:44 -0600

    new

--- js/indexsetter.js
+++ js/indexsetter.js
@@ -5,7 +5,7 @@ var indexContent = `
 		<li><a href="" id="phiLink">gaming</a><br></li>
 		<li>blogposts?</li>
 			<ol>
-				<p>--2022--</p>
+			<p>--2022--</p>
 				<li><a href="/main/blogposts/life29time04spot2022">spot of a lifetime</a></li>
 				<li><a href="/main/blogposts/happ25iest05girl2022">pepesito for inkie</a></li>
 				<li><a href="/main/blogposts/per16fect06ruiz2022">cloudy day sunny day</a></li>
@@ -21,10 +21,12 @@ var indexContent = `
 				<ol>	
 					<li><a href="/main/blogposts/stop24the11wheel2022">quien pudiera apaciguar esta angustia, esta locura?</a></li>
 				</ol>
-				<p>--2023--</p>
-				<li>
-					<a href="/main/blogposts/heyy13youuu01ASD2023"><img src="/media/yoshi1.webp" alt="first blogpost23"><img src="/media/yoshi2.webp" alt="first blogpost23"></a></li>
-					<li><a href="/main/blogposts/free22the01soul2023">to the very ends with you, in another lifetime</a></li>
+			<p>--2023--</p>
+				<li><a href="/main/blogposts/heyy13youuu01ASD2023"><img src="/media/yoshi1.webp" alt="first blogpost23"><img src="/media/yoshi2.webp" alt="first blogpost23"></a></li>
+				<li><a href="/main/blogposts/free22the01soul2023">to the very ends with you, in another lifetime</a></li>
+				<ol>
+				<li><a href="/main/blogposts/libera02el02alma2023">Hasta Las Últimas Profundidades Contigo (otra versión)</a></li>
+				</ol>
 			</ol>
 		<li><a href="/main/music">music</a><br></li>
 		<li><a href="/main/images">images</a><br></li>
