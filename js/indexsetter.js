20d524561ee165aede3a941521a207bb73860409
pepesito1 <pepesito1@users.noreply.github.com>
2023-02-28 20:40:39 -0600

    newpost

--- js/indexsetter.js
+++ js/indexsetter.js
@@ -26,6 +26,7 @@ var indexContent = `
 				<li><a href="/main/blogposts/free22the01soul2023">to the very ends with you, in another lifetime</a></li>
 				<ol>
 					<li><a href="/main/blogposts/libera02el02alma2023">Hasta Las Últimas Profundidades Contigo (otra versión)</a></li>
+					<li><a href="/main/blogposts/dont28make02me202">to the heavens y de vuelta (versión alternativa)</a></li>
 				</ol>
 			</ol>
 		<li><a href="/main/music">music</a><br></li>
