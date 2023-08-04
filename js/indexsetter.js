66503533b8500dda6ebff8bcb457cea154fe2d79
pepesito1 <pepesito1@users.noreply.github.com>
2023-08-03 23:48:58 -0600

    update n new

--- js/indexsetter.js
+++ js/indexsetter.js
@@ -3,6 +3,9 @@ var indexContent = `
 		<li><a href="/">homepage</a></li>
 		<li><a href="/main/aboutme">about me</a></li>
 		<li><a href="" id="phiLink">gaming</a><br></li>
+			<ul>
+				<li><div id="forRainbowText"></div></li>
+			</ul>
 		<li><a href="/main/music">music</a><br></li>
 		<li><a href="/main/images">images</a><br></li>
 		<li><a href="https://drive.google.com/drive/folders/1sMv4B3EMVO4MVUFqsiIwzHgkO3SZWOg1?usp=sharing">"To The Person I Loved The Most" por Lester Papadopoulos</a><br></li>
@@ -22,14 +25,14 @@ var colorsArray = [ //the colors have to be strings
     '63c6f7', //blue
     'ff00ff', //purple
 ];
-var gay ='wow, im a nice guy'; //set string, in this case for the text thatll have the href thing
+var gay ='A Quick Little Quirky Note About My Gaming List'; //set string, in this case for the text thatll have the href thing
 var lengthForText = gay.length; //grab the length, for the for loop
 var e = 0; //variable to loop through the array
 
 for (var i = 0; i <= lengthForText; i++){
     var substr = gay.substr(i,1); //split the text, letter by letter
     var result = substr.fontcolor(colorsArray[e]); //change the colors of the splitted letters
-    document.getElementById("forRainbowText").innerHTML += "<a href='/main/blogposts/iapologize22forprettymuch11everything2022'>" + result + "</a>"; //add the letters one by one
+    document.getElementById("forRainbowText").innerHTML += "<a href='/main/gamingAp'>" + result + "</a>"; //add the letters one by one
     if (e === colorsArray.length - 1){ //this means that if the loop for the colors reached its end, then itll start again
         e = 0;
     } else {
